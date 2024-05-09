08511ae19596d2fea85e4f6a45ed01c9bdf0c805
badx86 <badlolpro@gmail.com>
2024-05-09 21:04:44 +0400

    add Lesson Exceptions & pdf file

---
+++ Introduction/Exceptions.py
@@ -0,0 +1,20 @@
+# Handling exception errors: Try-except
+# new_list = [2, 4, 6, 'California']
+#
+# for element in new_list:
+#     try:
+#         print(element / 2)
+#     except:
+#         print("The element is not a number!")
+
+
+# While-Break
+n = 4
+while n > 0:
+    print(n)
+    n -= 1
+
+    if n == 2:
+        break
+
+print("Loop ended")
